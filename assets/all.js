519cd4af41f17685443285bd3919144295d4f22e
Daniel Gavrilov <dgavrilov95@gmail.com>
2015-11-14 11:51:16 +0000

    Combine all javascript in one file (assets/all.js)

---
+++ assets/all.js
@@ -0,0 +1,15 @@
+---
+---
+
+/* Bootstrap */
+
+{% include_relative bower_components/bootstrap-sass/assets/javascripts/bootstrap/transition.js %}
+{% include_relative bower_components/bootstrap-sass/assets/javascripts/bootstrap/collapse.js %}
+
+/* Typer */
+
+{% include_relative js/typed.min.js %}
+
+/* Main script */
+
+{% include_relative js/main.js %}
