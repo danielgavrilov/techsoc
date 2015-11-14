a14bb1dfc89ef78b0fee87dd077803dc1f2d6c07
Daniel Gavrilov <dgavrilov95@gmail.com>
2015-11-14 11:52:22 +0000

    Use FastClick to make navigation much snappier on mobile
    “…mobile browsers will wait approximately 300ms from the time that you
    tap the button to fire the click event. The reason for this is that the
    browser is waiting to see if you are actually performing a double tap.”
    
    FastClick removes this from our navigation (not rest of site, for
    accessibility reasons), making it way more snappier.

--- assets/all.js
+++ assets/all.js
@@ -6,6 +6,10 @@
 {% include_relative bower_components/bootstrap-sass/assets/javascripts/bootstrap/transition.js %}
 {% include_relative bower_components/bootstrap-sass/assets/javascripts/bootstrap/collapse.js %}
 
+/* FastClick */
+
+{% include_relative bower_components/fastclick/lib/fastclick.js %}
+
 /* Typer */
 
 {% include_relative js/typed.min.js %}
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,15 @@ console.log("Hi there fellow developer");
 
 $(function(){
 
-    $("#typer").typed({
-        strings: ["technology?", "socials?", "hackathons?", "projects?", "taking part?"],
-        typeSpeed: 100,
-        backDelay: 1000,
-        loop: false,
-        contentType: 'html', // or text
-        // defaults to false for infinite loop
-        loopCount: false
-    });
+  FastClick.attach($(".site-nav")[0]);
+
+  $("#typer").typed({
+    strings: ["technology?", "socials?", "hackathons?", "projects?", "taking part?"],
+    typeSpeed: 100,
+    backDelay: 1000,
+    loop: false,
+    contentType: 'html', // or text
+    // defaults to false for infinite loop
+    loopCount: false
+  });
 });
