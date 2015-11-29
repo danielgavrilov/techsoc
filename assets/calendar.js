3ddbfd17fa6bfc432b94fd9bb948a4194beefadd
Daniel Gavrilov <dgavrilov95@gmail.com>
2015-11-29 19:05:42 +0000

    Add calendar view on Events page

---
+++ assets/calendar.js
@@ -0,0 +1,73 @@
+---
+---
+
+{% comment %}
+
+This is a separate dependency because it weighs a whole lot, and it shouldn't be
+included anywhere else apart from the events page.
+
+{% endcomment %}
+
+{% include_relative bower_components/moment/min/moment.min.js %}
+{% include_relative bower_components/fullcalendar/dist/fullcalendar.min.js %}
+{% include_relative bower_components/fullcalendar/dist/lang/en-gb.js %}
+
+
+function eventTransform(event) {
+  return {
+    title: (event.label ? "[" + event.label + "] " : "") + event.title,
+    start: event.start_time,
+    end: event.end_time,
+    url: event.url
+  };
+}
+
+function getCalendarEvents() {
+  return $.getJSON("{{ '/data/events.json' | prepend: site.baseurl }}")
+    .then(function(events) {
+      return events.map(eventTransform);
+    });
+}
+
+function showCalendar() {
+  return getCalendarEvents()
+    .then(function(events) {
+
+      var $calendar = $("#calendar");
+      var $spinner = $calendar.find(".sk-circle");
+
+      $calendar.fullCalendar({
+        events: events,
+        header: {
+          left: "title",
+          center: "",
+          right: "today agendaWeek,month prev,next"
+        },
+        lang: "en-gb",
+        allDaySlot: false,
+        defaultView: "agendaWeek",
+        scrollTime: "12:00:00",
+        theme: "techsoc",
+        timeFormat: "HH:mm",
+        slotLabelFormat: "HH:mm",
+        columnFormat: "dddd",
+        aspectRatio: 16/9,
+        eventAfterAllRender: function() {
+          $spinner.remove();
+        }
+      });
+    })
+}
+
+$(function(){
+
+  var $calendar = $("#calendar");
+  var $calendarButton = $(".calendar-view-button");
+
+  if ($calendar.hasClass("active")) {
+    showCalendar();
+  } else {
+    $calendarButton.one("shown.bs.tab", showCalendar);
+  }
+
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 $.fn.collapse.Constructor.TRANSITION_DURATION = 200;
 
+function loadScript(url) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+  document.body.appendChild(script);
+  return script;
+}
+
+function loadCalendarScript() {
+  return loadScript("{{ '/assets/calendar.js' | prepend: site.baseurl }}");
+}
+
 $(function(){
 
   FastClick.attach($(".site-nav")[0]);
@@ -13,6 +25,16 @@ $(function(){
     // defaults to false for infinite loop
     loopCount: false
   });
+
+  var $calendar = $("#calendar");
+  var $calendarButton = $(".calendar-view-button");
+
+  if ($calendar.hasClass("active")) {
+    loadCalendarScript();
+  } else {
+    $calendarButton.one("shown.bs.tab", loadCalendarScript);
+  }
+
 });
 
 console.log("Oh, Hi there. Like looking under the hood? Us too!\n\nFind some looking-under-hoods events at {{ '/events' | prepend: site.baseurl | prepend: site.url }} and come along!")
