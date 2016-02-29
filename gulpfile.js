5edd32337577ef61f95274edc1c0dd47b59bc4e6
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-02-29 20:37:27 +0000

    Disable Jekyll incremental rebuild
    because it doesn’t correctly detect which files to rebuild

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var messages = {
  */
 gulp.task('jekyll-build', function(done) {
   browserSync.notify(messages.jekyllBuild);
-  return cp.spawn('bundle', 'exec jekyll build --incremental --config _config.yml,_config_dev.yml'.split(' '), {
+  return cp.spawn('bundle', 'exec jekyll build --config _config.yml,_config_dev.yml'.split(' '), {
     stdio: 'inherit'
   }).on('close', done);
 });
